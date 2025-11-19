253a48928fc3f783fc9e8f1bc307b4643a05ab7b
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-11-19 09:53:26 -0600

    imported Mediapipe libraries and edited html and css for canvas

--- sketch.js
+++ sketch.js
@@ -2,6 +2,11 @@
 // Jiya Khalsa Bangar
 // 17 November,Tuesday
 //
+
+// Sources:
+// https://mediapipe.readthedocs.io/en/latest/solutions/hands.html
+
+
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
