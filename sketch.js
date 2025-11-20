b83500edbe1f0f670a77b02e721740d954173a35
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-11-20 09:34:52 -0600

    started making the circle the user can manipulate

--- sketch.js
+++ sketch.js
@@ -5,10 +5,37 @@
 
 // Sources:
 // https://mediapipe.readthedocs.io/en/latest/solutions/hands.html
+//https://www.w3schools.com/graphics/canvas_drawing.asp
 
 
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+// grabs elements by using this function which is used to call an already present HTML element
+const video = document.getElementById("webcam");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// make circle
+
+class Circle {
+  constructor(x, y, color, radius) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.radius = radius;
+  }
+  
+  draw(ctx, canvasWidth, canvasHeight) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight); // clears the screen before drawing anything
+    ctx.beginPath();
+    ctx.arc(this.x, this,this.y, this.radius, Math.PI * 2); //makes the circle that the user will manipulate
+    ctx.fill(this.color);
+    ctx.closePath();
+  }
+}
+
+// 
 
 
 function setup() {
@@ -19,3 +46,4 @@ function draw() {
   background(220);
   circle(mouseX, mouseY, 20);
 }
+
