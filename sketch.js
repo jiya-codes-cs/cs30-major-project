3b6ea839c4aac83525df2de7fbde9b792b808c07
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-11-25 09:54:33 -0600

    added the letters around circle for display

--- sketch.js
+++ sketch.js
@@ -30,15 +30,95 @@ class Circle {
   draw(ctx, canvasWidth, canvasHeight) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight); // clears the screen before drawing anything
     ctx.beginPath();
-    ctx.arc(this.x, this,this.y, this.radius, Math.PI * 2); //makes the circle that the user will manipulate
-    ctx.fill(this.color);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2); //makes the circle that the user will manipulate
+    ctx.fillStyle = this.color;
+    ctx.fill();
     ctx.closePath();
   }
 }
 
 // 
+class LetterManager {
+  constructor() {
+    this.letters = [];
+    this.circleRadius = 140;
+  }
 
+  setLetters(list) {
+    this.letters = list;
+  }
 
+  drawLetters(centerX, centerY) {
+    let total = this.letters.length;
+    let angleStep = Math.PI * 2 / total;
+
+    let i = 0;
+    while(i < total) {
+      let angle = i * angleStep - Math.PI / 2;
+
+      let x = centerX + Math.cos(angle) * this.circleRadius;
+      let y = centerY + Math.sin(angle) * this.circleRadius;
+
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText(this.letters[i], x, y);
+ 
+      i = i + 1;
+    }
+  }
+}
+
+class App {
+  constructor() {
+    this.circle = new Circle(canvasWidth / 2, canvasHeight / 2, 40, "red");
+    this.LetterManager = new LetterManager();
+  }
+
+  setTargetPosition(newX, newY) {
+    this.circle.x = newX;
+    this.circle.y = newY;
+  }
+
+  setLetters(list) {
+    this.LetterManager.setLetters(list);
+  }
+
+  drawEverything() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.circle.draw();
+
+    this.LetterManager.drawLetters(this.circle.x, this.circle.y);
+  }
+}
+
+//
+let app = new App();
+
+//
+canvas.onmousemove = handleMouseMove;
+
+// used this fucntion for size and position of an HTML element relative to the browser's current visible screen 
+function handleMouseMove() {
+  let rect = canvas.getBoundingClientRect();
+  let mouseX = window.clientX - rect.left;
+  let mouseY = window.clientY - rect.top;
+ 
+  app.setTargetPosition(mouseX, mouseY);
+}
+
+// Demo check
+canvas.onclick = handleClick;
+ 
+function handleClick() {
+  app.setLetters([
+    "A", "B", "C", "D", "E", "F", "G",
+    "H", "I", "J", "K", "L", "M", "N",
+    "O", "P", "Q", "R", "S", "T", "U",
+    "V", "W", "X", "Y", "Z"
+  ]);
+}
+
+// this is a function that runs just before the screen so that it helps before redrawing the screen which allows a smoother transition  
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
